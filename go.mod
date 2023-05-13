ac863071d68cba430ca4cb4a92658723aff5baca
Maurício Antunes <mauricio.abreua@gmail.com>
2023-05-13 09:21:13 -0300

    Create a todo app to show SRE practices

--- go.mod
+++ go.mod
@@ -2,12 +2,19 @@ module github.com/mauricioabreu/sre-app
 
 go 1.20
 
-require github.com/labstack/echo/v4 v4.10.2
+require (
+	github.com/labstack/echo/v4 v4.10.2
+	gorm.io/driver/sqlite v1.5.0
+	gorm.io/gorm v1.25.1
+)
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
--- main.go
+++ main.go
@@ -4,12 +4,70 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	if err != nil {
+		e.Logger.Fatal(err.Error())
+	}
+
+	db.AutoMigrate(&Todo{})
+
+	todoHandler := TodoHandler{db: db}
+
+	e.GET("/todos", todoHandler.GetTodos)
+	e.POST("/todos", todoHandler.CreateTodo)
+	e.DELETE("/todos/:id", todoHandler.DeleteTodo)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+type Todo struct {
+	gorm.Model
+	Description string `json:"Description"`
+	Done        bool   `json:"Done"`
+}
+
+type TodoHandler struct {
+	db *gorm.DB
+}
+
+func (t *TodoHandler) GetTodos(c echo.Context) error {
+	var todos []Todo
+	if err := t.db.Find(&todos).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, todos)
+}
+
+func (t *TodoHandler) CreateTodo(c echo.Context) error {
+	todo := new(Todo)
+	if err := c.Bind(todo); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if err := t.db.Create(&todo).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusCreated, todo)
+}
+
+func (t *TodoHandler) DeleteTodo(c echo.Context) error {
+	id := c.Param("id")
+	result := t.db.Delete(&Todo{}, id)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, result.Error.Error())
+	}
+	if result.RowsAffected == 0 {
+		return c.NoContent(http.StatusNotFound)
+	}
+
+	return c.NoContent(http.StatusNoContent)
 }
